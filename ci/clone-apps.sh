97fb461135dc986490c932fd28cd8c94522cfd1b
karmoy <javimartin@vivaldi.net>
2023-01-02 12:21:01 +0100

    vulver a meter hrms

--- ci/clone-apps.sh
+++ ci/clone-apps.sh
@@ -18,6 +18,6 @@
 
 # Following are public repositories.
 
-git clone --depth 1 --branch main https://github.com/frappe/hrms.git repos/hrms
 git clone --depth 1 --branch main https://github.com/castlecraft/cfe repos/castlecraft
 git clone --depth 1 --branch main https://github.com/castlecraft/microsoft_integration repos/microsoft_integration
+git clone --depth 1 --branch main https://github.com/frappe/hrms.git repos/hrms
